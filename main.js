8c34b942edf4b7f1c932c87cecbfaa471093d8a2
Ivan Morozov <ivan.morozov@yandex.ru>
2019-03-03 23:40:44 +0300

    Почти финал

---
+++ main.js
@@ -0,0 +1,39 @@
+console.log("hello");
+
+const API = "https://ru.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=";
+const ouput = document.querySelector('.output');
+let input = document.querySelector('#requestInput');
+
+document.querySelector('.butt').addEventListener('click', ()=>{
+  getData(input.value);
+
+})
+
+function getData(request)
+{
+   return axios.get(`${API}${request}`).then(response => {     
+    // результат запроса       
+    return response;
+  }).then(value => {
+    // обрабатываем принятые данные (объект)
+    return value.data.query.search;
+   }).then(result =>
+    {
+      // отображаем данные
+      Object.keys(result).map(i => {
+        
+        // создаем поля для отображения результатов поиска
+        let snippet = document.createElement('div');
+        let resultCard = document.createElement('div');
+        // задаем им классы
+        snippet.classList.add('snippet');
+        resultCard.classList.add('resultCard');
+        // записываем результаты в поля
+        snippet.innerHTML = result[i].snippet;
+        // создаем "дерево" вывода
+        resultCard.appendChild(snippet);
+        ouput.appendChild(resultCard);
+      });      
+    }
+   );
+}
